7efcb190389521d1fc3b46fb7a601b00d37ef4a0
JakubOCloud <kubaojrzynski.cloud@gmail.com>
2026-04-27 08:50:38 +0200

    Changes before applying waf module

--- 1.AWS_static_website/Part2/terraform_static_site/modules/waf/main.tf
+++ 1.AWS_static_website/Part2/terraform_static_site/modules/waf/main.tf
@@ -1,16 +1,12 @@
 resource "aws_wafv2_web_acl" "this" {
-  provider = aws.us_east_1
 
   name        = "request-limit"
   description = "Limits requests from each source ip to 100 requests/min"
   scope       = "CLOUDFRONT"
+  tags        = var.tags
 
   default_action {
     allow {}
-  }
-
-  tags = {
-    tags = var.tags
   }
 
   visibility_config {
--- 1.AWS_static_website/Part2/terraform_static_site/provider.tf
+++ 1.AWS_static_website/Part2/terraform_static_site/provider.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 provider "aws" {
-  region = "us-east-1"
   alias  = "us_east_1"
+  region = "us-east-1"
 }
