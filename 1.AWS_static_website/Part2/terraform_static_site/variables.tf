30376dc5492685515d92613a9e33042b6e2568e5
JakubOCloud <kubaojrzynski.cloud@gmail.com>
2026-04-28 11:35:00 +0200

    applying changes for tflint

--- 1.AWS_static_website/Part2/terraform_static_site/variables.tf
+++ 1.AWS_static_website/Part2/terraform_static_site/variables.tf
@@ -1,8 +1,11 @@
 variable "bucket_name" {
   description = "S3 bucket name"
+  type        = string
 }
 
 variable "tags" {
+  type = map(string)
+
   default = {
     project = "static-website"
   }
