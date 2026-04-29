53e360d782f93fc722b77a818ec922ce49bcda5e
JakubOCloud <kubaojrzynski.cloud@gmail.com>
2026-04-29 16:03:16 +0200

    Update main.tf

--- 1.AWS_static_website/Part2/terraform_static_site/modules/waf/main.tf
+++ 1.AWS_static_website/Part2/terraform_static_site/modules/waf/main.tf
@@ -25,8 +25,9 @@ resource "aws_wafv2_web_acl" "this" {
 
     statement {
       rate_based_statement {
-        limit              = 100
-        aggregate_key_type = "IP"
+        limit                 = 100
+        aggregate_key_type    = "IP"
+        evaluation_window_sec = 60
       }
     }
 
