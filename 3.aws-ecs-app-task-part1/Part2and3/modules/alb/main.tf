3fcd39da7efbc4918027eeb6d6c2668fbfa07b89
JakubOCloud <kubaojrzynski.cloud@gmail.com>
2026-05-05 11:55:35 +0200

    task3 part3 added retention

--- 3.aws-ecs-app-task-part1/Part2and3/modules/alb/main.tf
+++ 3.aws-ecs-app-task-part1/Part2and3/modules/alb/main.tf
@@ -34,7 +34,7 @@ resource "aws_lb_target_group" "this" {
     unhealthy_threshold = 2
     timeout             = 5
     interval            = 30
-    path                = "/health"
+    path                = "/"
     matcher             = "200"
   }
 
--- 3.aws-ecs-app-task-part1/Part2and3/modules/cloudwatch/main.tf
+++ 3.aws-ecs-app-task-part1/Part2and3/modules/cloudwatch/main.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "this" {
-  name = var.log_group_name
-
-  tags = var.tags
+  name              = var.log_group_name
+  retention_in_days = 14
+  tags              = var.tags
 }
