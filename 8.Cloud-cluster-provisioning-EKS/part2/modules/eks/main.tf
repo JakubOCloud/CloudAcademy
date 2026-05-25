60ff1ae5616179cc409debe83c93c24dac1d6220
JakubOCloud <kubaojrzynski.cloud@gmail.com>
2026-05-25 16:48:45 +0200

    rewriting main.tf for eks

--- 8.Cloud-cluster-provisioning-EKS/part2/modules/eks/main.tf
+++ 8.Cloud-cluster-provisioning-EKS/part2/modules/eks/main.tf
@@ -1,43 +1,68 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+resource "aws_iam_role" "eks_cluster_role" {
 
-  cluster_name    = "demo-eks"
-  cluster_version = "1.35"
+  name = "demo-eks-cluster-role"
 
-  authentication_mode = "API_AND_CONFIG_MAP"
-
-  enable_cluster_creator_admin_permissions = true
-
-  access_entries = {
-    admin = {
-      principal_arn = "arn:aws:iam::366183011726:role/aws-reserved/sso.amazonaws.com/eu-central-1/AWSReservedSSO_AdministratorAccess_c6c4e2f33ef77df6"
-
-      policy_associations = {
-        admin = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-
-          access_scope = {
-            type = "cluster"
-          }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
         }
+        Action = "sts:AssumeRole"
       }
-    }
-  }
+    ]
+  })
 
-  cluster_endpoint_public_access = true
-
-  vpc_id     = var.vpc_id
-  subnet_ids = var.subnet_ids
-
-  eks_managed_node_groups = {
-    workers = {
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
-  }
   tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_eks_cluster" "this" {
+  name     = "demo-eks"
+  version  = "1.35"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
+  vpc_config {
+    subnet_ids              = var.subnet_ids
+    endpoint_public_access  = true
+    endpoint_private_access = false
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
+
+  tags = var.tags
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "demo-eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = ec2.amazonaws.com
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
