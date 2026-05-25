60f2f86c9050f2b75a478fe8167085d2b4e03b6e
JakubOCloud <kubaojrzynski.cloud@gmail.com>
2026-05-25 12:03:17 +0200

    added kubernetes provider

--- 8.Cloud-cluster-provisioning-EKS/part2/modules/eks/main.tf
+++ 8.Cloud-cluster-provisioning-EKS/part2/modules/eks/main.tf
@@ -1,4 +1,20 @@
+data "aws_eks_cluster" "eks" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
 module "eks" {
+
+
   source  = "terraform-aws-modules/eks/aws"
   version = "19.21.0"
 
