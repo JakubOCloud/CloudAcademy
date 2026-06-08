e3b219f5d81fbab011d4d5fcdc76d9375c26d8ce
JakubOCloud <kubaojrzynski.cloud@gmail.com>
2026-06-08 15:57:11 +0200

    ec2 endpoint and vpc config changes

--- 9.k8s_payment_app/terraform/modules/eks/main.tf
+++ 9.k8s_payment_app/terraform/modules/eks/main.tf
@@ -67,7 +67,9 @@ resource "aws_eks_cluster" "this" {
   role_arn = aws_iam_role.cluster.arn
 
   vpc_config {
-    subnet_ids = var.private_subnet_ids
+    subnet_ids              = var.private_subnet_ids
+    endpoint_private_access = true
+    endpoint_public_access  = true
   }
 
   depends_on = [
--- 9.k8s_payment_app/terraform/modules/vpc/vpc_endpoints.tf
+++ 9.k8s_payment_app/terraform/modules/vpc/vpc_endpoints.tf
@@ -98,3 +98,17 @@ resource "aws_vpc_endpoint" "eks" {
 
   private_dns_enabled = true
 }
+
+resource "aws_vpc_endpoint" "ec2" {
+  vpc_id            = aws_vpc.this.id
+  service_name      = "com.amazonaws.eu-central-1.ec2"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids = aws_subnet.private_app[*].id
+
+  security_group_ids = [
+    aws_security_group.vpc_endpoints.id
+  ]
+
+  private_dns_enabled = true
+}
