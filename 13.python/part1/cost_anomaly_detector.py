105a7ae8eeb899909cadff7532a7fba59961aa92
JakubOCloud <kubaojrzynski.cloud@gmail.com>
2026-07-14 14:41:09 +0200

    argument handle, basic functionality

--- 13.python/part1/cost_anomaly_detector.py
+++ 13.python/part1/cost_anomaly_detector.py
@@ -1,1 +1,31 @@
-print("Starter")
+import argparse
+import pandas as pd
+import os
+
+def load_data(file_path):
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File '{file_path}' does not exist.")
+
+    df = pd.read_csv(file_path)
+
+    return df
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Cloud Cost Anomaly Detector"
+    )
+
+    parser.add_argument(
+        "--input",
+        required=True,
+        help="Path to CSV file"
+    )
+
+    args = parser.parse_args()
+
+    df = load_data(args.input)
+
+    print(df.head())
+
+if __name__ == "__main__":
+    main()
