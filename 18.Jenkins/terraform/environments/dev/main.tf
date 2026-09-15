d3b70077dda53a55b0e6163b641a82b47f2f855a
JakubOCloud <kubaojrzynski.cloud@gmail.com>
2026-09-15 16:12:29 +0200

    initializing ecs

--- 18.Jenkins/terraform/environments/dev/main.tf
+++ 18.Jenkins/terraform/environments/dev/main.tf
@@ -36,3 +36,23 @@ module "monitoring" {
   environment       = var.environment
   retention_in_days = 14
 }
+
+module "ecs" {
+  source = "../../modules/ecs"
+
+  environment           = var.environment
+  vpc_id                = module.networking.vpc_id
+  private_subnet_ids    = module.networking.private_subnet_ids
+  alb_security_group_id = module.alb.alb_security_group_id
+  target_group_arn      = module.alb.target_group_arn
+  ecr_repository_url    = module.ecr.repository_url
+  image_tag             = "bootstrap-1"
+  execution_role_arn    = module.iam.ecs_execution_role_arn
+  task_role_arn         = module.iam.ecs_task_role_arn
+  log_group_name        = module.monitoring.ecs_log_group_name
+
+  desired_count  = 1
+  cpu            = 256
+  memory         = 512
+  container_port = 3000
+}
