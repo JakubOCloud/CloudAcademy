27569138812574f7936717f85ea3dccc75e45b62
JakubOCloud <kubaojrzynski.cloud@gmail.com>
2026-09-23 11:32:35 +0200

    changes for precreated ecr

--- 18.Jenkins/terraform/environments/dev/main.tf
+++ 18.Jenkins/terraform/environments/dev/main.tf
@@ -6,12 +6,6 @@ module "networking" {
   availability_zones = var.availability_zones
 }
 
-module "ecr" {
-  source = "../../modules/ecr"
-
-  environment = var.environment
-}
-
 module "alb" {
   source = "../../modules/alb"
 
@@ -27,7 +21,7 @@ module "iam" {
   source = "../../modules/iam"
 
   environment        = var.environment
-  ecr_repository_arn = module.ecr.repository_arn
+  ecr_repository_arn = "arn:aws:ecr:eu-central-1:366183011726:repository/task-management-api-dev"
   secret_arn         = module.secrets.secret_arn
 }
 
@@ -46,7 +40,7 @@ module "ecs" {
   private_subnet_ids    = module.networking.private_subnet_ids
   alb_security_group_id = module.alb.alb_security_group_id
   target_group_arn      = module.alb.target_group_arn
-  ecr_repository_url    = module.ecr.repository_url
+  ecr_repository_url    = "366183011726.dkr.ecr.eu-central-1.amazonaws.com/task-management-api-dev"
   image_tag             = var.image_tag
   execution_role_arn    = module.iam.ecs_execution_role_arn
   task_role_arn         = module.iam.ecs_task_role_arn
